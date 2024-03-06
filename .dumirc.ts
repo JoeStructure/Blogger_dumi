77c74f6a1fbd75d0ec24a9b809c609eab1d48f6e
[200~1017913943@qq.com <1017913943@qq.com>
2024-03-06 22:54:32 +0800

    test 202403062254

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'dumi';
 
-const repo = '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'
+const repo = 'BLOGGER'
 export default defineConfig({
   base: `/${repo}/`,
   publicPath: `/${repo}/`,
