e986f1957aa96f75d6cf0826efcef044e93cf42b
zhanglefei <Joe.zhang@keeson.com>
2024-03-06 21:12:38 +0800

    init 自动化部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'Blogger_dumi'
 export default defineConfig({
+  base: repo,
+  publicPath: repo,
   themeConfig: {
     name: 'LokfBlogger',
   },
